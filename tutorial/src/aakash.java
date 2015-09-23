1403d479df1ef66ebdc3991649f7658379b4eec7
vishnu <vishnu.saini216@gmail.com>
2015-09-23 14:53:54 +0530

    second commit

--- tutorial/src/aakash.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedInputStream;
-import java.io.IOException;
-
-public class aakash {
-	public static void main(String[] args) throws IOException {
-		BufferedInputStream br = new BufferedInputStream(System.in);
-		System.out.println();
-	}
-}
